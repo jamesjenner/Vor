3a8fd6929a85998b8d17168ca5344fa8753c8af8
james-jenner <james.jenner@ventyx.abb.com>
2014-08-22 13:05:01 +1000

    Reworked test to show required data on jenkins job

--- node/testJenkins.js
+++ node/testJenkins.js
@@ -5,90 +5,108 @@ var jenkinsapi = require('./lib/jenkins');
 
 var jenkins = jenkinsapi.init("http://awsjenkins.ventyx.abb.com:8080");
 
-jenkins.all_jobs(function(err, data) {
-  if(err) {
-    console.log("ERROR (all jobs): " + err);
-    return;
-  }
-
-  console.log('all jobs, count: ' + data.length);
-});
-
 jenkins.job_info('/kitchen_ellipse', function(err, data) {
   if(err) {
     console.log("ERROR (job info kitchen_ellipse): " + err);
     return;
   }
 
-  console.log("job info kitchen_ellipse: " + data);
-});
-
-jenkins.last_success('/kitchen_ellipse', function(err, data) {
-  if(err) {
-    console.log("last_success kitchen_ellipse: ERROR (last success): " + err);
-    return;
+  console.log("\t display name: " + data.displayName);
+  console.log("\t  description: " + data.description);
+  // console.log("\t         name: " + data.name);
+  // console.log("\t          url: " + data.url);
+  // console.log("\t    buildable: " + data.buildable);
+  // console.log("\t        color: " + data.color);
+  // console.log("\tHealth Report: " + data.healthReport[0].description);
+  // console.log("\t     icon URL: " + data.healthReport[0].iconUrl);
+  console.log("\t        score: " + data.healthReport[0].score);
+  console.log("\t       builds: ");
+  for(var i = 0; i < data.builds.length && i < 6; i++) {
+    (function(buildNbr) {
+      getJobResult(buildNbr, function(result, building, progress) {
+        if(building) {
+          console.log("\t               " + buildNbr + " -> " + progress + "%");
+        } else {
+          console.log("\t               " + buildNbr + " : " + result);
+        }
+      });
+    })(data.builds[i].number);
   }
-
-  console.log("last_success kitchen_ellipse: " + data);
-});
-
-jenkins.last_build_info('/kitchen_ellipse', function(err, data) {
-  if(err) {
-    console.log("last build info kitchen_ellipse: ERROR (last build info): " + data.statusCode);
-    return;
-  }
-
-  console.log("last build info kitchen_ellipse: " + data);
-});
-
-jenkins.last_build_report('/kitchen_ellipse', function(err, data) {
-
-  if(err) {
-    console.log("last build report kitchen_ellipse: ERROR (last build report): " + data.statusCode);
-    return;
-  }
-
-  console.log("last build report kitchen_ellipse: " + data);
-});
-
-jenkins.last_result('/kitchen_ellipse', function(err, data) {
-
-  if(err) {
-    console.log("last result kitchen_ellipse: ERROR (last result): " + data.statusCode);
-    return;
-  }
-
-  // console.log("last result kitchen_ellipse: " + JSON.stringify(data, null, " "));
-  var date = new Date(data.timestamp);
-  var estDuration = convertMS(data.estimatedDuration);
-  
-  console.log("last result kitchen_ellipse: " + 
-              "building: " + data.building + 
-              " fullDisplayName: " + data.fullDisplayName + 
-              " number: #" + data.number + 
-              " started: " + date 
-              + " complete: " + Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100) + "%"
-              + " result: " + data.result);
   
 });
 
-function convertMS(ms) {
-  var d, h, m, s;
-  s = Math.floor(ms / 1000);
-  m = Math.floor(s / 60);
-  s = s % 60;
-  h = Math.floor(m / 60);
-  m = m % 60;
-  d = Math.floor(h / 24);
-  h = h % 24;
-  return { d: d, h: h, m: m, s: s };
-};
+function getJobResult(jobNbr, callback) {
+  jenkins.build_info('/kitchen_ellipse', jobNbr, function(err, data) {
+    if(!err && callback !== undefined) {
+      callback(data.result, data.building, Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100));
+    }
+  });
+}
 
-jenkins.build_info('/kitchen_ellipse', 5357, function(err, data) {
-  if(err) {
-    console.log("build_info kicthen_ellipse 5357: ERROR (build info): " + data.statusCode);
-    return;
-  }
+//jenkins.last_success('/kitchen_ellipse', function(err, data) {
+//  if(err) {
+//    console.log("last_success kitchen_ellipse: ERROR (last success): " + err);
+//    return;
+//  }
+//
+//  console.log("last_success kitchen_ellipse: " + data);
+//});
 
-  console.log("build_info kicthen_ellipse 5357: " + data);
-});
+//jenkins.last_build_info('/kitchen_ellipse', function(err, data) {
+//  if(err) {
+//    console.log("last build info kitchen_ellipse: ERROR (last build info): " + data.statusCode);
+//    return;
+//  }
+//
+//  console.log("  last build info " + 
+//              "building: " + data.building + 
+//              " fullDisplayName: " + data.fullDisplayName + 
+//              " number: #" + data.number + 
+//              " started: " + new Date(data.timestamp) + 
+//              " complete: " + Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100) + "%" + 
+//              " result: " + data.result);
+//});
+//
+//jenkins.last_build_report('/kitchen_ellipse', function(err, data) {
+//
+//  if(err) {
+//    console.log("last build report kitchen_ellipse: ERROR (last build report): " + data.statusCode);
+//    return;
+//  }
+//
+//  console.log("last build report " + 
+//              "building: " + data.building + 
+//              " fullDisplayName: " + data.fullDisplayName + 
+//              " number: #" + data.number + 
+//              " started: " + new Date(data.timestamp) +
+//              " complete: " + Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100) + "%" +
+//              " result: " + data.result);
+//});
+//
+//jenkins.last_result('/kitchen_ellipse', function(err, data) {
+//
+//  if(err) {
+//    console.log("last result kitchen_ellipse: ERROR (last result): " + data.statusCode);
+//    return;
+//  }
+//
+//  // console.log("last result kitchen_ellipse: " + JSON.stringify(data, null, " "));
+//  
+//  console.log("      last result " + 
+//              "building: " + data.building + 
+//              " fullDisplayName: " + data.fullDisplayName + 
+//              " number: #" + data.number + 
+//              " started: " + new Date(data.timestamp) +
+//              " complete: " + Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100) + "%" +
+//              " result: " + data.result);
+//});
+//
+//jenkins.all_jobs(function(err, data) {
+//  if(err) {
+//    console.log("ERROR (all jobs): " + err);
+//    return;
+//  }
+//
+//  console.log('all jobs, count: ' + data.length);
+//});
+//
