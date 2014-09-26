22556023772bac8e6ac47e061387573196904858
james-jenner <james.jenner@ventyx.abb.com>
2014-09-26 18:00:54 +1000

    Styling and adding panels

--- main.js
+++ main.js
@@ -42,40 +42,15 @@ function addPanel() {
              '        <label class="col-md-4 control-label" for="title">Title</label> ' +
              '        <div class="col-md-6"> ' +
              '          <input id="title" name="title" type="text" placeholder="The title of the new panel" class="form-control input-md"> ' +
-//             '        <span class="help-block">Here goes your name</span>' +
              '        </div> ' +
              '      </div> ' +
              '      <div class="form-group"> ' +
-             '        <label class="col-md-4 control-label" for="icon">Icon</label> ' +
-//             '        <div class="col-md-4">' + 
-             '        <div class="col-md-4 fuelux">' + 
-    
-//             '          <div class="list-group">' +
-//             '            <a href="#" class="list-group-item active">' +
-//             '              <p class="list-group-item-heading">List group item heading</p>' +
-//             '              <p class="list-group-item-text"><i class="fa fa-users fa-lg"></i></p>' +
-//             '            </a>' +
-//             '            <a href="#" class="list-group-item">' +
-//             '              <p class="list-group-item-text"><i class="fa fa-users fa-lg"></i></p>' +
-//             '            </a>' +
-//             '          </div>' +
-
-//             '          <div class="btn-group selectlist" data-resize="auto" data-initialize="selectlist" id="mySelectlist">' +
-             '          <div class="btn-group selectlist" data-initialize="selectlist" id="iconSelectlist">' +
-             '            <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button">' +
-             '              <span class="selected-label"></span>' +
-             '              <span class="caret"></span>' +
-             '              <span class="sr-only">Toggle Dropdown</span>' +
-             '            </button>' +
-             '            <ul class="dropdown-menu" role="menu">' +
-             '              <li data-value="a"><a href="#"><i class="fa fa-users fa-lg"></i></a></li>' +
-             '              <li data-value="b"><a href="#"><i class="fa fa-camera fa-lg"></i></a></li>' +
-             '            </ul>' +
-             '            <input class="hidden hidden-field" name="mySelectlist" readonly="readonly" aria-hidden="true" type="text"/>' +
-             '          </div>' +
-    
+             '        <label class="col-md-4 control-label" for="icon">Inbuilt Icons</label> ' +
+             '        <div class="col-md-4">' + 
+             '          <button id="selectPanelIcon" class="btn btn-default iconpicker" data-iconset="fontawesome" role="iconpicker"></button>' +
              '        </div>' +
              '      </div>' +
+    
              '    </form>' +
              '  </div>' +
              '</div>',
@@ -84,11 +59,29 @@ function addPanel() {
         label: "Create",
         className: "",
         callback: function() {
-          
+          var title = $('#title').val();
+          var icon = $('#selectPanelIcon input').val();
+
+          insertPanel(title, icon);
         }
       }
     }
   });
+  
+$('#selectPanelIcon').iconpicker({ 
+    iconset: 'fontawesome',
+    icon: 'fa-key', 
+    rows: 10,
+    cols: 10,
+    placement: 'bottom',
+});
+
+$('#selectPanelIcon').on('change', function(e) { 
+    console.log(e.icon);
+});
+
+$('#selectPanelIcon').iconpicker('setIcon', 'fa-group');
+  
 }
 
 function reloadScreen() {
@@ -99,4 +92,67 @@ function editSettings() {
 
 function toggleConfigMode() {
   $(".configButtons").fadeToggle("fast", function() {});
+}
+
+function insertPanel(title, icon, column) {
+  title = ((title != null && title !== undefined) ? title : "");
+  icon = ((icon != null && icon !== undefined) ? icon : "fa-group");
+  column = ((column != null && column !== undefined) ? column : 1);
+
+  $("#columnContainer" + column).append(
+    '<div id="panel' + title + '" class="panel">' +
+    '  <div class="panel-heading">' +
+    '      <div class="btn-group pull-right">' +
+    '        <button id="panelBtnAddWidget' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-plus "></i>' +
+    '        </button>' +
+    '        <button id="panelBtnRemoveWidget' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-minus"></i>' +
+    '        </button>' +
+    '        <button id="panelBtnDelete' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-trash"></i>' +
+    '        </button>' +
+    '        <button id="panelBtnMoveDown' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-arrow-down"></i>' +
+    '        </button>' +
+    '        <button id="panelBtnMoveUp' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-caret-up"></i>' +
+    '        </button>' +
+    '        <button id="panelBtnMoveLeft' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-arrow-left"></i>' +
+    '        </button>' +
+    '        <button id="panelBtnMoveRight' + title + '" type="button" class="configButtons btn btn-default btn-sml" style="display: block;">' +
+    '          <i class="fa fa-chevron-right"></i>' +
+    '        </button>' +
+    '      </div>' +
+    '    <i class="fa ' + icon + ' fa-lg"></i> ' + title +
+    '  </div>' +
+    '  <div class="panel-body widget-container">' +
+    '  </div>' +
+    '</div>'
+  );
+  
+  $('#panelBtnAddWidget' + title).on('click', function() {
+  });
+    
+  $('#panelBtnRemoveWidget' + title).on('click', function() {
+  });
+    
+  $('#panelBtnDelete' + title).on('click', deletePanel(title));
+    
+  $('#panelBtnMoveDown' + title).on('click', function() {
+  });
+    
+  $('#panelBtnMoveUp' + title).on('click', function() {
+  });
+    
+  $('#panelBtnMoveLeft' + title).on('click', function() {
+  });
+    
+  $('#panelBtnMoveRight' + title).on('click', function() {
+  });
+}
+
+function deletePanel(title) {
+  $('#panel' + title).remove();
 }
