375ded67e478fadb56c2376477e926ee0e4c1df8
James Jenner <james.g.jenner@gmail.com>
2014-11-09 14:52:45 +1000

    Migrated to bower, removed redundant components and implemented wizard

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* global $:false */
+/* global $:false,bootbox,document,BootBoxWiz */
 
 /*
  * Main javascript logic for index.html
@@ -50,7 +50,6 @@ function addPanel() {
              '          <button id="selectPanelIcon" class="btn btn-default iconpicker" data-iconset="fontawesome" role="iconpicker"></button>' +
              '        </div>' +
              '      </div>' +
-    
              '    </form>' +
              '  </div>' +
              '</div>',
@@ -69,11 +68,12 @@ function addPanel() {
   });
   
   $('#selectPanelIcon').iconpicker({ 
-      iconset: 'fontawesome',
-      icon: 'fa-key', 
-      rows: 10,
-      cols: 10,
-      placement: 'bottom',
+    iconset: 'fontawesome',
+    icon: 'fa-key', 
+    rows: 10,
+    cols: 10,
+    placement: 'bottom',
+    search: false,
   });
 
   $('#selectPanelIcon').on('change', function(e) { 
@@ -84,45 +84,223 @@ function addPanel() {
 }
 
 function addWidget(panelIdSelecter) {
-  bootbox.dialog({
-    title: "Add a Widget",
-    message: '<div class="row">  ' +
-             '  <div class="col-md-12"> ' +
-             '    <form class="form-horizontal"> ' +
-             '      <div class="form-group"> ' +
-             '        <label class="col-md-4 control-label" for="title">Title</label> ' +
-             '        <div class="col-md-6"> ' +
-             '          <input id="title" name="title" type="text" placeholder="The title of the new widget" class="form-control input-md"> ' +
-             '        </div> ' +
-             '      </div> ' +
-             '      <div class="form-group"> ' +
-             '        <label class="col-md-4 control-label" for="icon">Widget Type</label> ' +
-             '        <div class="col-md-4">' + 
-             '          <select class="selectpicker">' +     
-             '            <option>Gauge</option>' + 
-             '            <option>Value</option>' + 
-             '            <option>Donut Chart</option>' + 
-             '            <option>Burndown Chart</option>' + 
-             '          </select>' + 
-             '        </div>' +
-             '      </div>' +
-             '    </form>' +
-             '  </div>' +
-             '</div>',
-    buttons: {
-      success: {
-        label: "Create",
-        className: "",
-        callback: function() {
-          var title = $('#title').val();
-          // var icon = $('#selectPanelIcon input').val();
+//  bootbox.dialog({
+//    title: "Add a Widget",
+//    message: '<div class="row">  ' +
+//             '  <div class="col-md-12"> ' +
+//             '    <form class="form-horizontal"> ' +
+//             '      <div class="form-group"> ' +
+//             '        <label class="col-md-4 control-label" for="title">Title</label> ' +
+//             '        <div class="col-md-6"> ' +
+//             '          <input id="title" name="title" type="text" placeholder="The title of the new widget" class="form-control input-md"> ' +
+//             '        </div> ' +
+//             '      </div> ' +
+//             '      <div class="form-group"> ' +
+//             '        <label class="col-md-4 control-label" for="icon">Widget Type</label> ' +
+//             '        <div class="col-md-4">' + 
+//             '          <select class="selectpicker">' +     
+//             '            <option>Gauge</option>' + 
+//             '            <option>Value</option>' + 
+//             '            <option>Donut Chart</option>' + 
+//             '            <option>Burndown Chart</option>' + 
+//             '          </select>' + 
+//             '        </div>' +
+//             '      </div>' +
+//             '    </form>' +
+//             '  </div>' +
+//             '</div>',
+//    buttons: {
+//      success: {
+//        label: "Create",
+//        className: "",
+//        callback: function() {
+//          var title = $('#title').val();
+//          // var icon = $('#selectPanelIcon input').val();
+//
+//          // insertPanel(title, icon);
+//        }
+//      }
+//    }
+//  });
+//  $('.selectpicker').selectpicker();
 
-          // insertPanel(title, icon);
-        }
-      }
+  var addWidgetWizardDialog = new BootBoxWiz({
+    title: 'Add a Widget',
+    guideEnabled: false,
+    stepContent: [ {
+      type: BootBoxWiz.TYPE_FIXED,
+      title: "Step 1",
+      content:
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="widgetTitle">Title</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="widgetTitle" name="widgetTitle" type="text" placeholder="Enter the title for the widget" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div> ' +
+      '<div class="form-group">' +
+      '  <label class="col-md-4 control-label" for="widgetType">Type</label> ' +
+      '  <div class="col-md-4">' + 
+      '    <select name="widgetType" class="selectpicker">' +     
+      '      <option>Value</option>' + 
+      '      <option>State</option>' + 
+      '      <option>Graph</option>' + 
+      '      <option>Graph Over Time</option>' + 
+      '      <option>History</option>' + 
+      '    </select>' + 
+      '  </div>' +
+      '</div>' +
+      '<div class="form-group">' +
+      '  <label class="col-md-4 control-label" for="widgetDataSource">Data Source</label> ' +
+      '  <div class="col-md-4">' + 
+      '    <select name="widgetDataSource" class="selectpicker">' +     
+      '      <option>Version One</option>' + 
+      '      <option>Jenkins</option>' + 
+      '      <option>Hudson</option>' + 
+      '      <option>Salesforce.com</option>' + 
+      '      <option>Internal</option>' + 
+      '    </select>' + 
+      '  </div>' +
+      '</div>',
+    },
+    {
+      type: BootBoxWiz.TYPE_DYNAMIC,
+      title: "Step 2",
+      logic: addWidgetDialogWidgetTypeLogic,
+      titles: [
+        'Paypal',
+        'Credit Card',
+        'Electronic Funds Transfer',
+        'Bitcoin',
+      ],
+      content: [
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="paypalUserId">Paypal User Id</label>' +
+      '      <input name="paypalUserId" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter paypal user id" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardName">Name</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardName" name="creditCardName" type="text" placeholder="Enter the name on the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div> ' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardNumber">Credit Card Number</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardNumber" name="creditCardNumber" type="text" placeholder="Enter the number of the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardExpiry">Expiry Date</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardExpiry" name="creditCardExpiry" type="text" placeholder="Enter the expiry for the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardSecurity">Credit Card Security</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardSecurity" name="creditCardSecurity" type="text" placeholder="Enter the credit card security id" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>',
+
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftAccountName">Account Name</label>' +
+      '      <input name="eftAccountName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the name of your account" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftBSB">BSB</label>' +
+      '      <input name="eftBSB" maxlength="8" type="text" required="required" class="form-control" placeholder="Enter your bank\'s BSB" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftAccountNumber">Account Number</label>' +
+      '      <input name="eftAccountNumber" maxlength="30" type="text" required="required" class="form-control" placeholder="Enter your bank account" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftReference">Reference</label>' +
+      '      <input name="eftReference" maxlength="30" type="text" class="form-control" placeholder="Enter a reference" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="bitcoinTransId">To Address</label>' +
+      '      <input name="bitcoinTransId" maxlength="100" type="text" required="required" class="form-control" placeholder="Enter the transaction id" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+      ],
+    },
+    {
+      type: BootBoxWiz.TYPE_FIXED,
+      title: "Step 3",
+      content: 
+      '<div class="col-xs-12">' +
+      '  <label class="col-md-4 control-label" for="feedback">Feedback?</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="feedback" name="feedback" type="text" placeholder="Enter feedback on your transaction experiance" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>'
     }
+    ]
   });
+  addWidgetWizardDialog.launch();
   $('.selectpicker').selectpicker();
+}
+
+function addWidgetDialogWidgetTypeLogic(fields) {
+  var paymentType = ((fields.paymentType !== null && fields.paymentType !== undefined) ? fields.paymentType : 'Value');
+
+  switch(paymentType) {
+    case "Value":
+      return 0;
+      
+    case "State":
+      return 1;
+      
+    case "Graph":
+      return 2;
+      
+    case "Graph Over Time":
+      return 3;
+      
+    case "History":
+      return 4;
+  }
+
+  return 0;
+}
+
+function addWidgetDialogDataSourceLogic(fields) {
+  var paymentType = ((fields.paymentType !== null && fields.paymentType !== undefined) ? fields.paymentType : 'Version One');
+
+  switch(paymentType) {
+    case "Version One":
+      return 0;
+      
+    case "Jenkins":
+      return 1;
+      
+    case "Hudson":
+      return 2;
+      
+    case "Salesforce.com":
+      return 3;
+      
+    case "Internal":
+      return 4;
+  }
+
+  return 0;
 }
 
 function reloadScreen() {
