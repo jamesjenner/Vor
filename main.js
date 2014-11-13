e7b7ecd5343388e5c2516d2fe9eed925ad9ddfe3
James Jenner <james.g.jenner@gmail.com>
2014-11-13 22:41:28 +1000

    Reworked config navigation

--- main.js
+++ main.js
@@ -14,21 +14,89 @@ function configureApplication() {
 
 function addApplicationButtonListeners() {
   
+  $("body").on('change', '#toggleConfigMode', function() {
+    toggleConfigMode();
+  });
+  
+  $("body").on('click', '#viewHome', function() {
+    toggleButtonViewSelection('viewHome');
+    showPanelButton('addPanel');
+    displayPane("homePane");
+  });
+  
+  $("body").on('click', '#viewSettings', function() {
+    toggleButtonViewSelection('viewSettings');
+    hidePanelButton('addPanel');
+    displayPane("settingsPane");
+  });
+  
+  $("body").on('click', '#viewThemes', function() {
+    toggleButtonViewSelection('viewThemes');
+    hidePanelButton('addPanel');
+    displayPane("themesPane");
+  });
+    
+  $("body").on('click', '#viewContent', function() {
+    toggleButtonViewSelection('viewContent');
+    hidePanelButton('addPanel');
+    displayPane("bannerContentPane");
+  });
+    
+  $("#viewDataSources").on('click', function() {
+    // addDataSource();
+    toggleButtonViewSelection('viewDataSources');
+    hidePanelButton('addPanel');
+    displayPane("dataSourcesPane");
+  });
+  
   $("body").on('click', '#addPanel', function() {
     addPanel();
   });
-    
-  $("body").on('click', '#reloadScreen', function() {
-    reloadScreen();
-  });
-    
-  $("body").on('click', '#editSettings', function() {
-    editSettings();
-  });
-    
-  // $("#toggleConfigMode").onchange(function() {
-  $("body").on('change', '#toggleConfigMode', function() {
-    toggleConfigMode();
+}
+
+function toggleConfigMode() {
+  var currentPaneId = $('.activePane').attr('id');
+  
+  if(currentPaneId !== "") {
+    $("#viewHome").trigger('click');
+  }
+  
+  $(".configButtons").fadeToggle("slow", function() {}).toggleClass('displayButton').css("display","");
+}
+
+function toggleButtonViewSelection(buttonId) {
+  var previousId = $('.configButtonActive').attr('id');
+
+  $('#' + previousId).toggleClass('configButtonInactive');
+  $('#' + previousId).toggleClass('configButtonActive');
+  
+  $('#' + buttonId).toggleClass('configButtonInactive');
+  $('#' + buttonId).toggleClass('configButtonActive');
+  
+  // toggleClass('configButtonActive');
+  
+}
+
+function showPanelButton(buttonId) {
+  $('#' + buttonId).removeClass('hiddenButton');
+}
+
+function hidePanelButton(buttonId) {
+  $('#' + buttonId).addClass('hiddenButton');
+}
+
+function displayPane(paneId) {
+  var previousId = $('.activePane').attr('id');
+  
+  if(previousId === paneId) {
+    return;
+  }
+  
+  $('#' + previousId).fadeToggle("fast", function() {
+    $(this).toggleClass('activePane');
+    $('#' + paneId).fadeToggle("fast", function() {
+      $(this).toggleClass('activePane');
+    });
   });
 }
 
@@ -84,46 +152,6 @@ function addPanel() {
 }
 
 function addWidget(panelIdSelecter) {
-//  bootbox.dialog({
-//    title: "Add a Widget",
-//    message: '<div class="row">  ' +
-//             '  <div class="col-md-12"> ' +
-//             '    <form class="form-horizontal"> ' +
-//             '      <div class="form-group"> ' +
-//             '        <label class="col-md-4 control-label" for="title">Title</label> ' +
-//             '        <div class="col-md-6"> ' +
-//             '          <input id="title" name="title" type="text" placeholder="The title of the new widget" class="form-control input-md"> ' +
-//             '        </div> ' +
-//             '      </div> ' +
-//             '      <div class="form-group"> ' +
-//             '        <label class="col-md-4 control-label" for="icon">Widget Type</label> ' +
-//             '        <div class="col-md-4">' + 
-//             '          <select class="selectpicker">' +     
-//             '            <option>Gauge</option>' + 
-//             '            <option>Value</option>' + 
-//             '            <option>Donut Chart</option>' + 
-//             '            <option>Burndown Chart</option>' + 
-//             '          </select>' + 
-//             '        </div>' +
-//             '      </div>' +
-//             '    </form>' +
-//             '  </div>' +
-//             '</div>',
-//    buttons: {
-//      success: {
-//        label: "Create",
-//        className: "",
-//        callback: function() {
-//          var title = $('#title').val();
-//          // var icon = $('#selectPanelIcon input').val();
-//
-//          // insertPanel(title, icon);
-//        }
-//      }
-//    }
-//  });
-//  $('.selectpicker').selectpicker();
-
   var addWidgetWizardDialog = new BootBoxWiz({
     title: 'Add a Widget',
     guideEnabled: false,
@@ -177,8 +205,38 @@ function addWidget(panelIdSelecter) {
       '<div class="col-xs-12">' +
       '  <div class="col-md-12">' +
       '    <div class="form-group">' +
-      '      <label class="control-label" for="paypalUserId">Paypal User Id</label>' +
-      '      <input name="paypalUserId" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter paypal user id" />' +
+      '      <label class="control-label" for="v1HostName">Hostname</label>' +
+      '      <input name="v1HostName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the hostname" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Instance">Instance</label>' +
+      '      <input name="v1Instance" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the version one instance" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1UserName">Username</label>' +
+      '      <input name="v1UserName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter username" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Password">Password</label>' +
+      '      <input name="v1Password" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the password" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Port">Port</label>' +
+      '      <input name="v1Port" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the port" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Protocol">Protocol</label>' +
+      '      <input name="v1Protocol" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the protocol (http|https)" />' +
       '    </div>' +
       '  </div>' +
       '</div>',
@@ -303,14 +361,169 @@ function addWidgetDialogDataSourceLogic(fields) {
   return 0;
 }
 
-function reloadScreen() {
+function addDataSource(panelIdSelecter) {
+  var addWidgetWizardDialog = new BootBoxWiz({
+    title: 'Add a Data Source',
+    guideEnabled: false,
+    stepContent: [ {
+      type: BootBoxWiz.TYPE_FIXED,
+      title: "Step 1",
+      content:
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="dataSourceName">Name</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="dataSourceName" name="dataSourceName" type="text" placeholder="Enter the name for the data source" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div> ' +
+      '<div class="form-group">' +
+      '  <label class="col-md-4 control-label" for="dataSourceType">Type</label> ' +
+      '  <div class="col-md-4">' + 
+      '    <select name="dataSourceType" class="selectpicker">' +     
+      '      <option>Version One</option>' + 
+      '      <option>Jenkins</option>' + 
+      '      <option>Hudson</option>' + 
+      '      <option>Salesforce.com</option>' + 
+      '      <option>Internal</option>' + 
+      '    </select>' + 
+      '  </div>' +
+      '</div>',
+    },
+    {
+      type: BootBoxWiz.TYPE_DYNAMIC,
+      title: "Step 2",
+      logic: addDataSourceDialogSourceLogic,
+      titles: [
+        'Version One',
+        'Jenkins',
+        'Hudson',
+        'Salesforce.com',
+        'Internal',
+      ],
+      content: [
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1HostName">Hostname</label>' +
+      '      <input name="v1HostName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the hostname" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Instance">Instance</label>' +
+      '      <input name="v1Instance" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the version one instance" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1UserName">Username</label>' +
+      '      <input name="v1UserName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter username" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Password">Password</label>' +
+      '      <input name="v1Password" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the password" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Port">Port</label>' +
+      '      <input name="v1Port" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the port" />' +
+      '    </div>' +
+      '  </div>' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="v1Protocol">Protocol</label>' +
+      '      <input name="v1Protocol" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the protocol (http|https)" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardName">Name</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardName" name="creditCardName" type="text" placeholder="Enter the name on the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div> ' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardNumber">Credit Card Number</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardNumber" name="creditCardNumber" type="text" placeholder="Enter the number of the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardExpiry">Expiry Date</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardExpiry" name="creditCardExpiry" type="text" placeholder="Enter the expiry for the credit card" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>' +
+      '<div class="form-group"> ' +
+      '  <label class="col-md-4 control-label" for="creditCardSecurity">Credit Card Security</label> ' +
+      '  <div class="col-md-6"> ' +
+      '    <input id="creditCardSecurity" name="creditCardSecurity" type="text" placeholder="Enter the credit card security id" class="form-control input-md" autofocus> ' +
+      '  </div> ' +
+      '</div>',
+
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftAccountName">Account Name</label>' +
+      '      <input name="eftAccountName" maxlength="200" type="text" required="required" class="form-control" placeholder="Enter the name of your account" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftBSB">BSB</label>' +
+      '      <input name="eftBSB" maxlength="8" type="text" required="required" class="form-control" placeholder="Enter your bank\'s BSB" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftAccountNumber">Account Number</label>' +
+      '      <input name="eftAccountNumber" maxlength="30" type="text" required="required" class="form-control" placeholder="Enter your bank account" />' +
+      '    </div>' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="eftReference">Reference</label>' +
+      '      <input name="eftReference" maxlength="30" type="text" class="form-control" placeholder="Enter a reference" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+
+      '<div class="col-xs-12">' +
+      '  <div class="col-md-12">' +
+      '    <div class="form-group">' +
+      '      <label class="control-label" for="bitcoinTransId">To Address</label>' +
+      '      <input name="bitcoinTransId" maxlength="100" type="text" required="required" class="form-control" placeholder="Enter the transaction id" />' +
+      '    </div>' +
+      '  </div>' +
+      '</div>',
+      ],
+    }
+    ]
+  });
+  addWidgetWizardDialog.launch();
+  $('.selectpicker').selectpicker();
 }
 
-function editSettings() {
-}
+function addDataSourceDialogSourceLogic() {
+  var paymentType = ((fields.paymentType !== null && fields.paymentType !== undefined) ? fields.paymentType : 'Version One');
 
-function toggleConfigMode() {
-  $(".configButtons").fadeToggle("fast", function() {});
+  switch(paymentType) {
+    case "Version One":
+      return 0;
+      
+    case "Jenkins":
+      return 1;
+      
+    case "Hudson":
+      return 2;
+      
+    case "Salesforce.com":
+      return 3;
+      
+    case "Internal":
+      return 4;
+  }
+
+  return 0;
 }
 
 function insertPanel(title, icon, column) {
