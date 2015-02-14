f536ea3c9645c5c0856de82250f09eec7423fa7d
James Jenner <james.g.jenner@gmail.com>
2015-02-14 17:28:39 +1000

    Completed gauge

--- conceptual/js/listGauge.js
+++ conceptual/js/listGauge.js
@@ -7,166 +7,125 @@ ListGauge.VALUES_PERCENTAGE = "valuesPercentage";
 ListGauge.VALUES_INT = "valuesActual";
 ListGauge.VALUES_TEXT = "valuesText";
 
+ListGauge.DIRECTION_BOTTOM_UP = "directionUp";
+ListGauge.DIRECTION_TOP_DOWN = "directionDown";
+
+ListGauge.ALIGN_LEFT = "alignLeft";
+ListGauge.ALIGN_RIGHT = "alignRight";
+ListGauge.ALIGN_CENTRE = "alignCentre";
+
 function ListGauge(options) {
+  var d, i;
   options = options || {};
 
+  this.depth = ((options.depth !== null && options.depth !== undefined) ? options.depth : 5);
+  this.direction = ((options.direction !== null && options.direction !== undefined) ? options.direction : ListGauge.DIRECTION_TOP_DOWN);
   this.valueType = ((options.valueType !== null && options.valueType !== undefined) ? options.valueType : ListGauge.VALUES_INT);
+  this.align = ((options.align !== null && options.align !== undefined) ? options.align : ListGauge.ALIGN_RIGHT);
 
   this.defaultColor = ((options.defaultColor !== null && options.defaultColor !== undefined) ? options.defaultColor : "green");
   
-  this.setValue(options.value, false);
-  
   this.textDisplayMode = ((options.textDisplayMode !== null && options.textDisplayMode !== undefined) ? options.textDisplayMode : ListGauge.DISPLAY_VALUE);
-  this.textSize = ((options.textSize !== null && options.textSize !== undefined) ? options.textSize : 5);
+  this.textSize = ((options.textSize !== null && options.textSize !== undefined) ? options.textSize : 1.5);
   this.textFontName = ((options.textFontName !== null && options.textFontName !== undefined) ? options.textFontName : "sans-serif");
   this.textFontWeight = ((options.textFontWeight !== null && options.textFontWeight !== undefined) ? options.textFontWeight : "bold");
   
   this.appendTo = ((options.appendTo !== null && options.appendTo !== undefined) ? options.appendTo : "body");
-  
-  this.values = [];
-//  this.width = ((options.width !== null && options.width !== undefined) ? options.width : 300);
-//  this.height = ((options.height !== null && options.height !== undefined) ? options.height : 300);
 
+  this.values = ((options.values !== null && options.values !== undefined) ? options.values : []);
+  
+  if (this.values.length < this.depth) {
+    for (d = this.values.length; d < this.depth; d++) {
+      this.values[d] = new ListValue("", "");
+    }
+  }
 
   // Create the SVG container, and apply a transform such that the origin is the
   // center of the canvas. This way, we don't need to position arcs individually.
   // setup the viewbox so that the size changes automatically
 
-  var svg = d3.select("#" + this.appendTo).append("svg")
+  this.svg = d3.select("#" + this.appendTo).append("svg")
     .attr("id", "numWidget")
-    .attr("width", this.width)
-    .attr("height", this.height)
+    .attr("width", "100px")
+    .attr("height", "100px")
     .attr("viewBox", "0 0 100 100")  // set viewBox to 200/200 so we can use 0 to 100 for sizing
     .attr("perserveAspectRatio", "meet")
     // .attr("perserveAspectRatio", "none")
     .append("g")
-    // .attr("transform", "translate(100, 100)")
-    ;  // do we need the translate? don't think so...
+  ;
 
-  // determin the true size of the svg element
-  //  var elem1 = document.getElementById(this.appendTo);
-  //  var style = window.getComputedStyle(elem1, null);
-  //  var svgBBox = svg.node().getBBox();
-  // svg = svg.append("g").attr("transform", "translate(" + parseFloat(style.width) / 2 + "," + parseFloat(style.height) / 2 + ")");
+  var height = (100 / this.depth);
+  var textAnchor = "start";
+  var xPos = 0;
+  switch(this.align) {
 
-  this.text = svg.append("text")
-      .attr("id", "numValue")
-      .attr("dx", "50%")
-      .attr("dy", "50%")
-      .text(this._getTextValue())
-      .attr("text-anchor", "middle")
-      .attr("dominant-baseline", "central")
-      .attr("font-family", this.textFontName)
-      .style("font-weight", this.textFontWeight)
-      .style("font-size", this.textSize + "em")
-      .attr("fill", this.defaultColor);
-
-/*
-      <text 
-          id="value"
-          dx="50%" 
-          dy="50%" 
-          text-anchor="middle" 
-          font-family="sans-serif"
-          font-weight="bold"
-          font-size="5em"
-          dominant-baseline="central"
-          fill="red">
-          9
-      </text>
- 
- */
-
-}
-
-ListGauge.prototype._getTextValue = function() {
-  var displayText = '';
-  
-  switch(this.textDisplayMode) {
-    case ListGauge.DISPLAY_PERCENTAGE:
-      displayText = this.valuePercentage * 100;
+    case ListGauge.ALIGN_LEFT:
+      textAnchor = "start";
+      xPos = 0;
       break;
-      
-    case ListGauge.DISPLAY_VALUE:
-    default:
-      displayText = this.value;
-  }
-  
-  return displayText;
-};
-
-ListGauge.prototype._determineForegroundColor = function() {
-  var foregroundColor = this.goodColor;
-
-  if(this.goodDirection === ListGauge.GOOD_LOW) {
-    if(this.value <= this.goodMargin) {
-      foregroundColor = this.goodColor;
-    } else if(this.value >= this.dangerMargin) {
-      foregroundColor = this.dangerColor;
-    } else {
-      foregroundColor = this.warningColor;
-    }
-  } else {
-    if(this.value >= this.target - this.goodMargin) {
-      foregroundColor = this.goodColor;
-    } else if(this.value <= this.target - this.dangerMargin) {
-      foregroundColor = this.dangerColor;
-    } else {
-      foregroundColor = this.warningColor;
-    }
+    case ListGauge.ALIGN_RIGHT:
+      textAnchor = "end";
+      xPos = 100;
+      break;
+    case ListGauge.ALIGN_CENTRE:
+      textAnchor = "middle";
+      xPos = 50;
+      break;
   }
 
-  return foregroundColor;
-};
+  this.text = [];
+  for(i = 0; i < this.depth; i++) {
+    console.log("value: " + this.values[i].value + " y pos: " + (height * i));
+    this.text[i] = this.svg.append("text")
+        .attr("id", this.appendTo + "numValue" + i)
+        .attr("x", xPos)
+        .attr("y", (height * i) + height / 2)
+        .attr("width", "100")
+        .text(this.values[i].value)
+        .attr("text-anchor", textAnchor)
+        .attr("dominant-baseline", "central")
+        .attr("font-family", this.textFontName)
+        .style("font-weight", this.textFontWeight)
+        .style("font-size", this.textSize + "em")
+        .attr("fill", this.defaultColor);
+  }
+}
 
 function ListValue(value, color) {
-  this.color = "white";
-  this.value = "";
+  this.color = color;
+  this.value = value;
 }
 
-ListGauge.prototype.pushValue(value, color) {
+ListGauge.prototype.pushValue = function(value, color) {
+  var i = 0, j = 0;
+  
+  if(this.values.length >= this.depth) {
+    this.shiftValue();
+  }
+  
   this.values.push(new ListValue(value, color));
-}
-
-ListGauge.prototype.popValue() {
-  this.values.pop();
-}
-
-ListGauge.prototype.redraw() {
-}
-
-ListGauge.prototype.setValue = function(position, newValue, newColor, redrawGauge) {
-  var oldValue = this.value;
-  var replacementValue = newValue;
-  this.value = ((newValue !== null && newValue !== undefined) ? newValue : 0);
   
-  redrawGauge = ((redrawGauge !== null && redrawGauge !== undefined) ? redrawGauge : true);
-  
-  var format = d3.format("f");
-      
-  if(this.valueType == ListGauge.VALUES_PERCENTAGE) {
-    oldValue = this.valuePercentage;
-    replacementValue = (this.value - this.minValue) / (this.maxValue - this.minValue);
-    format = d3.format(".0%");
-  }
-  
-  if(redrawGauge) {
-    (function(replacementValue, gaugeInst) {
-      gaugeInst.text.transition()
-        .duration(750)
+  if(this.direction === ListGauge.DIRECTION_BOTTOM_UP) {
+    for(i = 0; i < this.values.length; i++) {
+      this.text[i].transition()
+        .duration(0)
         .ease('linear')
-        .style("fill", gaugeInst._determineForegroundColor())
-        .tween('text', function() {
-          var ip = d3.interpolate(oldValue, replacementValue);
-          return function(t) {
-            var v = ip(t);
-            this.textContent = format(v);
-          };
-      });
-    })(replacementValue, this);
+        .style("fill", this.values[i].color)
+        .text(this.values[i].value);
+    }
+  } else {
+    for(i = 0, j = this.values.length - 1; i < this.values.length; i++, j--) {
+      this.text[j].transition()
+        .duration(0)
+        .ease('linear')
+        .style("fill", this.values[i].color)
+        .text(this.values[i].value);
+    }
   }
-  
-  // note: cannot bind this to the tween function, as this points to the selection of the tween
+};
+
+ListGauge.prototype.shiftValue = function() {
+  this.values.shift();
 };
 
 ListGauge.prototype.demo = function() {
@@ -174,11 +133,15 @@ ListGauge.prototype.demo = function() {
   // (identical to selection.call) so that we can encapsulate the logic for
   // tweening the arc in a separate function below.
   
+  this.lastValue = (Math.floor(Math.random() * (10 - 2)) + 2) * 1000;
+  this.colors = ["lightgray", "red", "green", "green", "green"]
+  
   setInterval(function() {
-    var min = 500;
-    var max = 1500;
+    var min = 0;
+    var max = 4;
     // calc a int random value between min and max
     var value = Math.floor(Math.random() * (max - min)) + min;
-    this.setValue(value);
-  }.bind(this), 1500);
+    
+    this.pushValue(++this.lastValue, this.colors[value]);
+  }.bind(this), Math.floor(Math.random() * (1500 - 750)) + 750);
 };
