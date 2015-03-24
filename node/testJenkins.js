6d1935efb88019856fca065dda091d197c941df6
james-jenner <james.jenner@ventyx.abb.com>
2015-03-24 13:06:40 +1000

    Added test results

--- node/testJenkins.js
+++ node/testJenkins.js
@@ -5,11 +5,15 @@ var jenkinsapi = require('./lib/jenkins');
 
 var jenkins = jenkinsapi.init("http://awsjenkins.ventyx.abb.com:8080");
 
-jenkins.job_info('/kitchen_ellipse', function(err, data) {
+// var target = "kitchen_ellipse";
+var target = "ellipse-unit-tests";
+
+jenkins.job_info('/' + target, function(err, data) {
   if(err) {
     console.log("ERROR (job info kitchen_ellipse): " + err);
     return;
   }
+//  console.log("data: " + JSON.stringify(data, null, ' '));
 
   console.log("\t display name: " + data.displayName);
   console.log("\t  description: " + data.description);
@@ -21,7 +25,9 @@ jenkins.job_info('/kitchen_ellipse', function(err, data) {
   // console.log("\t     icon URL: " + data.healthReport[0].iconUrl);
   console.log("\t        score: " + data.healthReport[0].score);
   console.log("\t       builds: ");
-  for(var i = 0; i < data.builds.length && i < 6; i++) {
+  
+  
+//  for(var i = 0; i < data.builds.length && i < 6; i++) {
     (function(buildNbr) {
       getJobResult(buildNbr, function(result, building, progress) {
         if(building) {
@@ -30,13 +36,22 @@ jenkins.job_info('/kitchen_ellipse', function(err, data) {
           console.log("\t               " + buildNbr + " : " + result);
         }
       });
-    })(data.builds[i].number);
-  }
+      })(data.builds[0].number);
+//    })(data.builds[i].number);
+//  }
   
 });
 
 function getJobResult(jobNbr, callback) {
-  jenkins.build_info('/kitchen_ellipse', jobNbr, function(err, data) {
+  jenkins.build_info('/' + target, jobNbr, function(err, data) {
+    console.log("\tFailed: " + data["actions"][6].failCount);
+    console.log("\tSkipped: " + data["actions"][6].skipCount);
+    console.log("\tPassed: " + 
+      (parseInt(data["actions"][6].totalCount) - 
+      (
+        parseInt(data["actions"][6].failCount) + parseInt(data["actions"][6].skipCount) 
+      ))
+    );
     if(!err && callback !== undefined) {
       callback(data.result, data.building, Math.round((new Date().getTime() - data.timestamp) / data.estimatedDuration * 100));
     }
